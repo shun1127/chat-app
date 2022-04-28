48887fc6a7bef9d5350f5c6c84ae32a3b888faf5
shun1127 <watashun1127@gmail.com>
2022-04-28 13:23:16 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :tests
-  devise_for :users
+  devise_for :user
   get 'messages/index'
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
